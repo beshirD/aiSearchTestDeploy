c2997d82d62d68d8f20a4c494933929778f171dd
beshirD <beshirdek@gmail.com>
2023-09-13 17:15:38 +0300

    add create bot table logic

--- src/pages/api/bots/index.ts
+++ src/pages/api/bots/index.ts
@@ -10,6 +10,80 @@ import { botValidationSchema } from 'validationSchema/bots';
 import { convertQueryToPrismaUtil, getOrderByOptions, parseQueryParams } from 'server/utils';
 import { getServerSession } from '@roq/nextjs';
 import { GetManyQueryOptions } from 'interfaces';
+import { createClient } from '@supabase/supabase-js';
+
+import { Pool } from 'pg';
+
+// Initialize the Supabase client with your Supabase URL and service role key
+const supabaseUrl = process.env.SUPABASE_BASE_URL;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabase = createClient(supabaseUrl, supabaseServiceKey);
+
+// Create a PostgreSQL connection pool
+const pool = new Pool({
+  connectionString: process.env.SUPABASE_DATABASE_URL, 
+});
+
+async function createTableForUser(apiKey:any) {
+  const tableName = `bot_${apiKey}`;
+
+  const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS "${tableName}" (
+      id bigserial,
+      content text null,
+      metadata jsonb null,
+      embedding public.vector null,
+      constraint "${tableName}_pkey" primary key (id)
+    );
+  `;
+
+  try {
+    const client = await pool.connect();
+    await client.query(createTableQuery);
+    client.release();
+  } catch (error) {
+    console.error('Error creating table:', error);
+  }
+}
+// Function to create the match_documents function
+async function createMatchDocumentsFunction(apiKey:any) {
+  const functionName = `bot_${apiKey}`;
+
+  const createFunctionQuery = `
+  create function "match_${functionName}" (
+    query_embedding vector (1536),
+    match_count int default null,
+    filter jsonb default '{}'
+  ) returns table (
+    id bigint,
+    content text,
+    metadata jsonb,
+    similarity float
+  ) language plpgsql as $$
+  #variable_conflict use_column
+  begin
+    return query
+    select
+      id,
+      content,
+      metadata,
+      1 - ("${functionName}".embedding <=> query_embedding) as similarity
+    from "${functionName}"
+    where metadata @> filter
+    order by "${functionName}".embedding <=> query_embedding
+    limit match_count;
+  end;
+  $$
+  `;
+
+  try {
+    const client = await pool.connect();
+    await client.query(createFunctionQuery);
+    client.release();
+  } catch (error) {
+    console.error(`Error creating function ${functionName}:`, error);
+  }
+}
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { roqUserId, user } = await getServerSession(req);
@@ -66,6 +140,8 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
     const data = await prisma.bot.create({
       data: {...body,organization_id:org.id},
     });
+    await createTableForUser(data.id);
+    await createMatchDocumentsFunction(data.id);
     await notificationHandlerMiddleware(req, data.id);
     return res.status(200).json(data);
   }
