ab734e29f9fe2c26346cc3fda9d752c0925ed5bb
beshirD <beshirdek@gmail.com>
2023-09-13 17:14:25 +0300

    update bot view page

--- src/components/search-page/index.tsx
+++ src/components/search-page/index.tsx
@@ -2,69 +2,18 @@ import { useState } from 'react';
 import axios from 'axios';
 import { Input, Button, Box, Heading, Text, Spinner, Flex, VStack, useToast } from '@chakra-ui/react';
 import { FileUpload } from '@roq/nextjs';
-const SearchPage = () => {
-  // const [selectedFile, setSelectedFile] = useState(null);
+const SearchPage = ({ apiKey }: { apiKey: string }) => {
   const [selectedUrl, setSelectedUrl] = useState('');
-  // const [question, setQuestion] = useState('');
-  // const [responses, setResponses] = useState([]);
-  // const [uploadLoading, setUploadLoading] = useState(false);
+
   const [urlLoading, setUrlLoading] = useState(false);
-  // const [searchLoading, setSearchLoading] = useState(false);
-  // const [isFileSelected, setIsFileSelected] = useState(false);
   const [isLinkSelected, setIsLinkSelected] = useState(false);
 
   const toast = useToast();
 
-  // const handleFileChange = (e: any) => {
-  //   setSelectedFile(e.target.files[0]);
-  //   setIsFileSelected(!!e.target.files[0]);
-  // };
   const handleLinkChanged = (e: any) => {
     setSelectedUrl(e.target.value);
     //todo add function that checks if the link is valid
   };
-
-  // const handleFileUpload = async () => {
-  //   if (!selectedFile) return;
-
-  //   const formData = new FormData();
-  //   formData.append('file', selectedFile);
-
-  //   try {
-  //     setUploadLoading(true);
-  //     await axios.post('/api/testupload/file_embedding_generator', formData, {
-  //       headers: {
-  //         'Content-Type': 'multipart/form-data',
-  //       },
-  //     });
-  //     // const url = 'https://roq-ai-search-mvp.vercel.app/';
-  //     // const loader = new PlaywrightWebBaseLoader(url);
-  //     // const docs = await loader.load();
-  //     // console.log(docs,"test docs");
-  //     console.log('File uploaded successfully');
-  //     toast({
-  //       title: 'Upload Successful',
-  //       status: 'success',
-  //       duration: 3000,
-  //       isClosable: true,
-  //       position: 'top-right',
-  //     });
-  //     setSelectedFile(null);
-  //     setIsFileSelected(false);
-  //   } catch (error) {
-  //     console.error('Error uploading file:', error);
-  //     toast({
-  //       title: 'Upload Error',
-  //       description: 'An error occurred while uploading the file.',
-  //       status: 'error',
-  //       duration: 3000,
-  //       isClosable: true,
-  //       position: 'top-right',
-  //     });
-  //   } finally {
-  //     setUploadLoading(false);
-  //   }
-  // };
 
   const handleLink = async () => {
     try {
@@ -93,31 +42,12 @@ const SearchPage = () => {
     }
   };
 
-  // const handleSearch = async () => {
-  //   console.log(question, 'the query here');
-
-  //   try {
-  //     setSearchLoading(true);
-  //     const requestBody = { query: question };
-  //     const response = await axios.post('/api/testupload/search', requestBody);
-  //     console.log(response, 'the response haha');
-  //     setResponses([response?.data?.data, ...responses]);
-  //   } catch (error) {
-  //     console.log('error:', error);
-  //   } finally {
-  //     setSearchLoading(false);
-  //   }
-  // };
-
-  // const clearResponses = () => {
-  //   setResponses([]);
-  //   setQuestion('');
-  // };
-
   return (
-    <Box p="4" maxH="full">
+    <Box maxH="full">
       <Box mt="4" border="1px" borderColor="gray.600" borderStyle="dashed" minH={120} p="4" borderRadius="md">
-        <Heading size="md">File Upload</Heading>
+        <Heading size="md" mb="2">
+          File Upload
+        </Heading>
 
         <FileUpload
           accept={['*']}
@@ -130,7 +60,7 @@ const SearchPage = () => {
 
                 const formData = new FormData();
                 formData.append('file', blob, name);
-                await axios.post('/api/testupload/file_embedding_generator', formData, {
+                await axios.post(`/api/upload/file_embedding_generator/${apiKey}`, formData, {
                   headers: {
                     'Content-Type': 'multipart/form-data',
                   },
--- src/pages/bots/view/[id]/index.tsx
+++ src/pages/bots/view/[id]/index.tsx
@@ -1,4 +1,20 @@
-import { Box, Center, Flex, Link, List, ListItem, Spinner, Stack, Text, Image, Button } from '@chakra-ui/react';
+import {
+  Box,
+  Center,
+  Flex,
+  Link,
+  List,
+  ListItem,
+  Spinner,
+  Stack,
+  Text,
+  Image,
+  Button,
+  Input,
+  InputGroup,
+  InputRightElement,
+  IconButton,
+} from '@chakra-ui/react';
 import Breadcrumbs from 'components/breadcrumb';
 import { Error } from 'components/error';
 import { FormListItem } from 'components/form-list-item';
@@ -21,10 +37,11 @@ import {
 } from '@roq/nextjs';
 import { UserPageTable } from 'components/user-page-table';
 import { EntityImage } from 'components/entity-image';
-import { FiEdit2 } from 'react-icons/fi';
+import { FiCheck, FiCopy, FiEdit2 } from 'react-icons/fi';
 
 import { getBotById } from 'apiSdk/bots';
 import { BotInterface } from 'interfaces/bot';
+import SearchPage from 'components/search-page';
 
 function BotViewPage() {
   const { hasAccess } = useAuthorizationApi();
@@ -37,7 +54,17 @@ function BotViewPage() {
         relations: ['organization'],
       }),
   );
+  const [showCheckIcon, setShowCheckIcon] = useState(false);
 
+  const copyText = () => {
+    const textToCopy = data.id;
+    navigator.clipboard.writeText(textToCopy);
+    setShowCheckIcon(true);
+
+    setTimeout(() => {
+      setShowCheckIcon(false);
+    }, 3000);
+  };
   const [deleteError, setDeleteError] = useState(null);
   const [createError, setCreateError] = useState(null);
 
@@ -115,8 +142,25 @@ function BotViewPage() {
                 <FormListItem label="Name" text={data?.name} />
 
                 <FormListItem label="Description" text={data?.description} />
+                <Box pt="3">
+                  <Text fontSize="2xl" fontWeight="bold" p="2">
+                    API Key
+                  </Text>
+                  <InputGroup>
+                    <Input value={data.id} p="2" />
+                    <InputRightElement>
+                      {showCheckIcon ? (
+                        <IconButton aria-label="Check" icon={<FiCheck />} size="sm" />
+                      ) : (
+                        <IconButton aria-label="Copy" icon={<FiCopy />} onClick={copyText} size="sm" />
+                      )}
+                    </InputRightElement>
+                  </InputGroup>
+                </Box>
               </List>
             </FormWrapper>
+
+            <SearchPage apiKey={data.id} />
           </>
         )}
       </Box>
